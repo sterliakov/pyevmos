1ebce7e37e8c77279955fdb13b338a6655402965
STerliakov <terlya.stas@gmail.com>
2022-09-13 02:47:27 +0300

    Store scripts in root folder only

---
+++ scripts/create_proto_files.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# NOTE: protoc is required
+set -euo pipefail
+cd evmos/proto
+
+I=$(pwd)/autogen/proto
+DEST_PY=$(pwd)/autogen/py/
+mkdir -p $DEST_PY
+cd $I
+
+protoc \
+    --python_betterproto_out=$DEST_PY \
+    -I $I \
+    $(find $I -iname "*.proto")
+
+touch $DEST_PY/__init__.py
--- scripts/get_proto_files.sh
+++ scripts/get_proto_files.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+cd evmos/proto
 MYFOLDER="$(pwd)/aautogen"
 
 # Ethermint
