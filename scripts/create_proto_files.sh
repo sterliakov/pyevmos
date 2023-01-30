78ae8e62ba924eacdc20c8e19fc93dfe388db2d1
STerliakov <terlya.stas@gmail.com>
2023-01-30 23:05:35 +0300

    Add patch for links

--- scripts/create_proto_files.sh
+++ scripts/create_proto_files.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 cd evmos/proto
 
+ROOT=$(pwd)/autogen
 I=$(pwd)/autogen/proto
 DEST_PY=$(pwd)/autogen/py/
 mkdir -p "$DEST_PY"
@@ -13,5 +14,8 @@ protoc \
     --python_betterproto_out="$DEST_PY" \
     -I "$I" \
     $(find "$I" -iname "*.proto")
-
 touch "$DEST_PY/__init__.py"
+
+cd "$ROOT"
+pre-commit run --all-files &>/dev/null || true
+git apply links.patch
